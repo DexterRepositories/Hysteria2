0c127f628841e0c01439ab5fa17e18c815ad062a
Dexter Eskalarte <139760444+DexterRepositories@users.noreply.github.com>
2023-09-06 07:46:33 +0800

    Update udp_test.go

--- core/client/udp_test.go
+++ core/client/udp_test.go
@@ -10,8 +10,8 @@ import (
 	"github.com/stretchr/testify/mock"
 	"go.uber.org/goleak"
 
-	coreErrs "github.com/apernet/hysteria/core/errors"
-	"github.com/apernet/hysteria/core/internal/protocol"
+	coreErrs "github.com/DexterRepositories/Hysteria2/core/errors"
+	"github.com/DexterRepositories/Hysteria2/core/internal/protocol"
 )
 
 func TestUDPSessionManager(t *testing.T) {

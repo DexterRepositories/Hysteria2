e2cf7930c754f0023b72846bbef1e13243cc6ef8
Dexter Eskalarte <139760444+DexterRepositories@users.noreply.github.com>
2023-09-06 07:49:19 +0800

    Update frag_test.go

--- core/internal/frag/frag_test.go
+++ core/internal/frag/frag_test.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/apernet/hysteria/core/internal/protocol"
+	"github.com/DexterRepositories/Hysteria2/core/internal/protocol"
 )
 
 func TestFragUDPMessage(t *testing.T) {

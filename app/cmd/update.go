aced1ec25fe8480c535bf6a6e4cd9bd1bb2d324f
Dexter Eskalarte <139760444+DexterRepositories@users.noreply.github.com>
2023-09-06 07:37:15 +0800

    Update update.go

--- app/cmd/update.go
+++ app/cmd/update.go
@@ -6,8 +6,8 @@ import (
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 
-	"github.com/apernet/hysteria/app/internal/utils"
-	"github.com/apernet/hysteria/core/client"
+	"github.com/DexterRepositories/Hysteria2/app/internal/utils"
+	"github.com/DexterRepositories/Hysteria2/core/client"
 )
 
 const (
